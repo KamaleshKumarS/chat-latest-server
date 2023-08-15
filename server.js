587cc565727a46b4cb09cb9ff0fe5699d1228cdf
unknown <kamaleshkumars1806@gmail.com>
2023-08-15 22:40:42 +0530

    withport

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors({
 	orgin:"*",
 }));
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.post('/register',(req,res)=>{
 	console.log(req.body)
 	if(req.body){
